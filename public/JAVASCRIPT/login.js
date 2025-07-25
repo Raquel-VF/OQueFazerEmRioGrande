04b0aa7cbaffb01117a5c7cf4527fc673547acb1
Raquel <raquelvargas0905@gmail.com>
2025-07-25 18:09:04 -0300

    login com google pelo celular

--- public/JAVASCRIPT/login.js
+++ public/JAVASCRIPT/login.js
@@ -1,44 +1,59 @@
-// Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+import { 
+  getAuth, 
+  signInWithPopup, 
+  signInWithRedirect, 
+  getRedirectResult, 
+  GoogleAuthProvider, 
+  FacebookAuthProvider 
+} from "firebase/auth";
 
-// Teu config aqui:
-const firebaseConfig = {
-  apiKey: "AIzaSyCmRdBJoQjNRcSQ6jBdpeLG07-Qr5O4N6Y",
-  authDomain: "o-que-fazer-em-rio-grand-4ca52.firebaseapp.com",
-  projectId: "o-que-fazer-em-rio-grand-4ca52",
-  storageBucket: "o-que-fazer-em-rio-grand-4ca52.appspot.com",
-  messagingSenderId: "1038923994330",
-  appId: "1:1038923994330:web:ca4cce7c73223896ddc1e3"
-};
+const auth = getAuth();
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Login com Google
+// Google
 document.querySelector('.google').addEventListener('click', async () => {
   const provider = new GoogleAuthProvider();
   try {
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-    alert(`Bem-vinda, ${user.displayName}`);
-    window.location.href = 'HTML/home.html';
+    if (isMobile) {
+      await signInWithRedirect(auth, provider);
+    } else {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      alert(`Bem-vinda, ${user.displayName}`);
+      window.location.href = 'HTML/home.html';
+    }
   } catch (error) {
     console.error(error);
     alert('Erro no login com Google');
   }
 });
 
-// Login com Facebook
+// Facebook
 document.querySelector('.facebook').addEventListener('click', async () => {
   const provider = new FacebookAuthProvider();
   try {
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-    alert(`Bem-vinda, ${user.displayName}`);
-    window.location.href = './HTML/home.html';
+    if (isMobile) {
+      await signInWithRedirect(auth, provider);
+    } else {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      alert(`Bem-vinda, ${user.displayName}`);
+      window.location.href = './HTML/home.html';
+    }
   } catch (error) {
     console.error(error);
     alert('Erro no login com Facebook');
   }
 });
+
+// Recuperar usuário depois de redirect
+getRedirectResult(auth)
+  .then((result) => {
+    if (result && result.user) {
+      alert(`Bem-vinda, ${result.user.displayName}`);
+      window.location.href = 'HTML/home.html';
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
